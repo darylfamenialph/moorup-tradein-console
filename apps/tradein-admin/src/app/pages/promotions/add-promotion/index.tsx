f71dadde3a0149e559a3cf4f95f0c1517211132e
jpentiniomoorup <154985430+jpentiniomoorup@users.noreply.github.com>
2024-11-12 07:09:32 +0800

    fix datepicker bug

--- apps/tradein-admin/src/app/pages/promotions/add-promotion/index.tsx
+++ apps/tradein-admin/src/app/pages/promotions/add-promotion/index.tsx
@@ -79,52 +79,73 @@ export function AddPromotionForm() {
   };
 
   const onSubmit = (values: any) => {
-    values.start_date = moment(values.start_date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
-    values.end_date = moment(values.end_date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
-    values.send_in_deadline = moment(values.send_in_deadline).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
-    values.payment_due_date = moment(values.payment_due_date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
+    values.start_date = moment(values.start_date)
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
+    values.end_date = moment(values.end_date)
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
+    values.send_in_deadline = moment(values.send_in_deadline)
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
+    values.payment_due_date = moment(values.payment_due_date)
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
     values.new_device_purchase_start_date = moment(
       values.new_device_purchase_start_date,
-    ).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
+    )
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
     values.new_device_purchase_end_date = moment(
       values.new_device_purchase_end_date,
-    ).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
-    values.claim_deadline = moment(values.claim_deadline).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toISOString();
+    )
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
+    values.claim_deadline = moment(values.claim_deadline)
+      .utc()
+      .set({
+        hour: 0,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      })
+      .toISOString();
     values.platform = activePlatform;
 
     setAddPromotionDetailsPayload(values);
@@ -146,27 +167,45 @@ export function AddPromotionForm() {
   );
 
   const handleStartDateChange = (date: Date | null) => {
-    formik.setFieldValue('start_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
-    formik.setFieldValue('end_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
+    formik.setFieldValue(
+      'start_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
+    formik.setFieldValue(
+      'end_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
   };
 
   const handleEndDateChange = (date: Date | null) => {
-    formik.setFieldValue('end_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
+    formik.setFieldValue(
+      'end_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
   };
 
   const handleStartDateOnBlur = () => {
@@ -180,12 +219,18 @@ export function AddPromotionForm() {
   };
 
   const handleDateChange = (fieldName: string, date: Date | null) => {
-    formik.setFieldValue(fieldName, moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
+    formik.setFieldValue(
+      fieldName,
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
   };
 
   const handleSendInDeadlineDateOnBlur = () => {
@@ -225,27 +270,45 @@ export function AddPromotionForm() {
   };
 
   const handleNewDevicePurchaseStartDateChange = (date: Date | null) => {
-    formik.setFieldValue('new_device_purchase_start_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
-    formik.setFieldValue('new_device_purchase_end_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
+    formik.setFieldValue(
+      'new_device_purchase_start_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
+    formik.setFieldValue(
+      'new_device_purchase_end_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
   };
 
   const handleNewDevicePurchaseEndDateChange = (date: Date | null) => {
-    formik.setFieldValue('new_device_purchase_end_date', moment(date).utc().set({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-     }).toDate());
+    formik.setFieldValue(
+      'new_device_purchase_end_date',
+      moment(date)
+        .utc()
+        .set({
+          hour: 0,
+          minute: 0,
+          second: 0,
+          millisecond: 0,
+        })
+        .toDate(),
+    );
   };
 
   const handleClaimDeadlineDateOnBlur = () => {
