620f05fb9cc9577ccce611c9f17f86bd0ea3fd86
jpentiniomoorup <154985430+jpentiniomoorup@users.noreply.github.com>
2024-12-16 09:35:34 +0800

    remove scrollbar in order item card

--- apps/tradein-admin/src/app/pages/order-management/edit-order/validation-offer.tsx
+++ apps/tradein-admin/src/app/pages/order-management/edit-order/validation-offer.tsx
@@ -93,7 +93,7 @@ const ValidationOffer = ({
   console.log('orderItems: ', orderItems);
 
   return (
-    <div className="grid min-[2560px]:grid-cols-4 sm:grid-cols-3 grid-cols-1 gap-2 p-2.5 items-start">
+    <div className="grid min-[2560px]:grid-cols-4 2xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-2 p-2.5 items-start">
       {orderItems?.map((item: OrderItems, idx) => {
         const { questions_answered = [] } = item;
 
