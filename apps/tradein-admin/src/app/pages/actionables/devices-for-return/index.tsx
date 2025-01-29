433fc1331275507116948400f1e5436f264af82f
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2025-01-29 08:05:21 +0800

    [MIPP-383] Customize Columns - Actionables -> Devices For Return

--- apps/tradein-admin/src/app/pages/actionables/devices-for-return/index.tsx
+++ apps/tradein-admin/src/app/pages/actionables/devices-for-return/index.tsx
@@ -1,11 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
+import { faSliders } from '@fortawesome/free-solid-svg-icons';
 import {
   ACTIONABLES_DEVICES_FOR_RETURN_COLUMNS,
   ACTIONS_COLUMN,
+  Column,
+  CustomizeColumns,
+  Divider,
+  IconButton,
+  MODAL_TYPES,
   OrderItemStatus,
   PageSubHeader,
   Pages,
+  SideModal,
   Table,
   actionablesDevicesForReturnParsingConfig,
   useAuth,
@@ -13,7 +20,7 @@ import {
   useOrder,
 } from '@tradein-admin/libs';
 import { isEmpty } from 'lodash';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 export function DevicesForReturnPage() {
@@ -27,12 +34,22 @@ export function DevicesForReturnPage() {
   const { isFetchingOrderItems, orderItems, isUpdatingOrderItem } = orderState;
   const { state: authState } = useAuth();
   const { activePlatform, userDetails } = authState;
-  const { setSearchTerm } = useCommon();
+  const { state: commonState, setSideModalState, setSearchTerm } = useCommon();
+  const { sideModalState } = commonState;
 
-  const headers = [
+  const customizedColumns = JSON.parse(localStorage.getItem('CC') || '{}');
+  const savedColumns =
+    customizedColumns[
+      MODAL_TYPES.CUSTOMIZE_COLUMNS_ACTIONABLES_DEVICES_FOR_RETURN
+    ];
+  const defaultColumns = [
     ...ACTIONABLES_DEVICES_FOR_RETURN_COLUMNS,
     ...ACTIONS_COLUMN,
   ];
+
+  const [headers, setHeaders] = useState<Column[]>(
+    savedColumns ?? defaultColumns,
+  );
 
   const filters = {
     page: Pages.DEVICES_FOR_RETURN,
@@ -86,9 +103,60 @@ export function DevicesForReturnPage() {
     };
   }, [activePlatform]);
 
+  const renderSideModalContent = () => {
+    switch (sideModalState.view) {
+      case MODAL_TYPES.CUSTOMIZE_COLUMNS_ACTIONABLES_FOLLOW_UP_RECYCLE_OFFER:
+        return (
+          <CustomizeColumns
+            storageKey={
+              MODAL_TYPES.CUSTOMIZE_COLUMNS_ACTIONABLES_DEVICES_FOR_RETURN
+            }
+            defaultColumns={headers}
+            onSave={(newColumns: Column[]) => {
+              setHeaders(newColumns);
+              setSideModalState({
+                ...sideModalState,
+                open: false,
+                view: null,
+              });
+            }}
+          />
+        );
+
+      default:
+        return;
+    }
+  };
+
+  const onCloseSideModal = () => {
+    setSideModalState({
+      ...sideModalState,
+      open: false,
+      view: null,
+    });
+  };
+
   return (
     <>
-      <PageSubHeader withSearch />
+      <PageSubHeader
+        withSearch
+        rightControls={
+          <>
+            <IconButton
+              tooltipLabel="Customize Columns"
+              icon={faSliders}
+              onClick={() => {
+                setSideModalState({
+                  ...sideModalState,
+                  open: true,
+                  view: MODAL_TYPES.CUSTOMIZE_COLUMNS_ACTIONABLES_FOLLOW_UP_RECYCLE_OFFER,
+                });
+              }}
+            />
+            <Divider />
+          </>
+        }
+      />
       <Table
         label="Devices For Return"
         isLoading={isFetchingOrderItems || isUpdatingOrderItem}
@@ -96,6 +164,9 @@ export function DevicesForReturnPage() {
         rows={formattedOrderItems || []}
         parsingConfig={actionablesDevicesForReturnParsingConfig}
       />
+      <SideModal isOpen={sideModalState?.open} onClose={onCloseSideModal}>
+        {renderSideModalContent()}
+      </SideModal>
     </>
   );
 }
