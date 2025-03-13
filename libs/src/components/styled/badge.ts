933fb71b841509201ca5a27dcdd21bf25347a6a8
Jenrielle Gaon <jenrielle.gaon@moorup.com.au>
2025-03-14 00:52:13 +0800

    [MIPP-520] Update tag color and allow dynamic props to badge component

--- libs/src/components/styled/badge.ts
+++ libs/src/components/styled/badge.ts
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
-export const Badge = styled.div`
+interface BadgeProps {
+  backgroundColor?: string;
+  textColor?: string;
+}
+
+export const Badge = styled.div<BadgeProps>`
   display: block;
   padding: 2px 8px;
-  background-color: #01463a;
-  color: white; 
+  background-color: ${props => props.backgroundColor || '#01463a'};
+  color: ${props => props.textColor || 'white'}; 
   width: fit-content;
   border-radius: 3px;
   font-size: 12px;
